7eab7be7ef32a5c6ab7aec34712c3686b0c0173b
GabrielHenB <ghbraz17@gmail.com>
2024-06-21 21:06:45 -0300

    feat: adds company search by id and company view

--- src/Pages/CompanyView.vue
+++ src/Pages/CompanyView.vue
@@ -1,16 +1,69 @@
 <script setup>
-//import {} from '@/api.js';
-//import {onMounted, ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import { URL_COMPANY, URL_COMPANY_GETALL } from '@/config.js';
+import {Fetcher} from '@/api.js';
+import SearchComponent from '@/Components/SearchComponent.vue';
+import LoadingItem from '@/Components/LoadingItem.vue';
+import Message from '@/Components/Message.vue';
 
-//const coisa = ref("");
-//const resposta = ref("");
-//const mostrarErro = ref(false);
+const dados = ref([]);
+const msg = ref("");
+const mostrarErro = ref(false);
+const loading = ref(false);
+
+onMounted(async () => {
+    const fetcher = new Fetcher();
+    loading.value = true;
+    try{
+        const response = await fetcher.useFetch(URL_COMPANY_GETALL);
+        console.log(response);
+        dados.value = response;
+    }catch(error){
+        console.error('Error fetching data:', error);
+        // Axios error has a message attribute
+        msg.value = " " + error.message;
+        mostrarErro.value = true;
+    }finally{
+        loading.value = false;
+        // Apenas para debug:
+        //dados.value = [{name: "SuperMart", email: "super"},{name: "Josjwcnwj", email: "sisxijwi"}];
+    }
+});
+
+function getSearch(data){
+    // Obtem os dados do Emit do SearchComponent e joga no ref dados.
+    dados.value = data;
+}
+
+function closeMsg(){
+  mostrarErro.value = false;
+}
+
 </script>
 
 <template>
 
 <main>
+    <div v-if="mostrarErro" class="warning">
+      <Message titulo="Erro!" @close="closeMsg">
+        Ocorreu um erro na requisição. <br>
+        <span class="text-danger">{{msg}}</span>
+      </Message>
+    </div>
     <div class="container">
+        <div class="row">
+            <div class="col-12 text-center m-4 p-2">
+                <SearchComponent :endpoint="URL_COMPANY" @result="getSearch" />
+            </div>
+        </div>
+        <div v-if="loading" class="text-center m-2 p-2"><LoadingItem></LoadingItem> Carregando...</div>
+        <div v-if="dados.length === 0 && !loading" class="fs-4 text-center py-4">Nenhuma empresa foi encontrada</div>
+        <div v-if="dados.length > 0" class="row">
+            <div class="col-12 mx-2 my-1 p-1" v-for="(item,index) of dados" :id="'empresa' + index"  >
+                <h4>{{ item.name }}</h4>
+                <p>{{ item.email }}</p>
+            </div>
+        </div>
         <div class="row">
             <div class="col-12 text-center m-4 p-2">
                 <img src="/construction.png" width="140px" alt="">
